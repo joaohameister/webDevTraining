0f656d073b55b2770f003067fd86ecd51f24d9db
joaohameister <joaolucioh@gmail.com>
2023-09-12 08:54:08 -0300

    API_v1 finished

---
+++ ToDo-API/Controllers/SuperHeroController.cs
@@ -0,0 +1,69 @@
+﻿using Dapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Data.SqlClient;
+
+namespace ToDo_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SuperHeroController : ControllerBase
+    {
+        private readonly IConfiguration _config;
+        public SuperHeroController(IConfiguration _config)
+        {
+            this._config = _config;
+        }
+        [HttpGet]
+        public async Task<ActionResult<List<SuperHero>>> GetAllSuperHeroes()
+        {
+            using var connection = new SqlConnection(_config.GetConnectionString("DefaultConnection"));
+            IEnumerable<SuperHero> heroes = await SelectAllHeroes(connection);
+            return Ok(heroes);
+
+        }
+
+        private static async Task<IEnumerable<SuperHero>> SelectAllHeroes(SqlConnection connection)
+        {
+            return await connection.QueryAsync<SuperHero>("select * from superHeroesdb");
+        }
+
+        [HttpGet("{heroId}")]
+        public async Task<ActionResult<List<SuperHero>>> GetHero(int heroId)
+        {
+            using var connection = new SqlConnection(_config.GetConnectionString("DefaultConnection"));
+            var hero = await connection.QueryFirstAsync<SuperHero>("select * from superHeroesdb where id=@Id",
+                new { Id = heroId });
+
+            return Ok(hero);
+
+        }
+        [HttpPost]
+        public async Task<ActionResult<List<SuperHero>>> CreateHero(SuperHero hero)
+        {
+            using var connection = new SqlConnection(_config.GetConnectionString("DefaultConnection"));
+            await connection.ExecuteAsync("insert into superHeroesdb(name, firstName, lastName, place) values(@Name, @FirstName, @LastName, @Place)", hero);
+            return Ok(await SelectAllHeroes(connection));
+
+        }
+        [HttpPut]
+        public async Task<ActionResult<List<SuperHero>>> UpdateHero(SuperHero hero)
+        {
+            using var connection = new SqlConnection(_config.GetConnectionString("DefaultConnection"));
+            await connection.ExecuteAsync("update superheroesdb set name=@Name, firstName=@FirstName, lastname=@LastName, place=@Place where id=@Id", hero);
+
+            return Ok(await SelectAllHeroes(connection));
+
+
+        }
+        [HttpDelete("{heroId}")]
+        public async Task<ActionResult<List<SuperHero>>> DeleteHero(int heroId)
+        {
+            using var connection = new SqlConnection(_config.GetConnectionString("DefaultConnection"));
+            await connection.ExecuteAsync("delete from superheroesdb where id=@Id", new { Id = heroId });
+
+            return Ok(await SelectAllHeroes(connection));
+        }
+    }
+        
+}
---
+++ ToDo-API/Controllers/WeatherForecastController.cs
@@ -0,0 +1,43 @@
+using Microsoft.AspNetCore.Mvc;
+namespace ToDo_API.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class WeatherForecastController : ControllerBase
+{
+    private static readonly string[] Summaries = new[]
+    {
+    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+};
+
+    private readonly ILogger<WeatherForecastController> _logger;
+
+    public WeatherForecastController(ILogger<WeatherForecastController> logger)
+    {
+        _logger = logger;
+    }
+
+    [HttpGet]
+    public IEnumerable<WeatherForecast> Get()
+    {
+        return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+        {
+            Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+            TemperatureC = Random.Shared.Next(-20, 55),
+            Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+        })
+        .ToArray();
+    }
+    [HttpGet("[action]")]
+
+    public int getCelsius2(int? t)
+    {
+        if (t.HasValue) {
+            return (int)t;
+        }
+        else
+        {
+            return Random.Shared.Next(-20, 55);
+        }
+    }
+}
